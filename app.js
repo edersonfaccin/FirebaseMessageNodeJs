9a8ded640e9d5f7f6b266d1b7e4933a6c11949d7
Ederson Faccin Frasson <edersonfrasson@gmail.com>
2020-07-20 10:31:33 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const fcm = require('fcm-notification')
 const FCM = new fcm('./util/pushapp-firebase-adminsdk.json')
 const tokens = ['dhlcUb4rS6GICJU9ZWc-UC:APA91bEm6U2aSgWHhV7tHGl98twb33DNNJmJCsGrnTx6xKrEvavC-lTp7sqBhh_xjHC-oLQjholgJdTbBYi0EQysTQEB0sh7PSE4e75V13f-Xy-RCzKFzkVkDIM4fCUxocY1-bvofS7k']
+
 /*
-fcm.initializeApp({
-    credential: fcm.credential.cert(FCM),
-    databaseURL: "https://pushapp-f3441.firebaseio.com"
-})*/
+o arquivo pushapp-firebase-adminsdk.json deve ser gerado no firebase console
+*/
 
 const message = {
     data: {
